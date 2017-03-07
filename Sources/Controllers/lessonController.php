db401f1dcf3adafaaa1477f3aab97dd14a56d7e8
Slawik <s.sivinyuk@gmail.com>
2017-03-07 23:20:17 +0200

    home 03.07

--- Sources/Controllers/lessonController.php
+++ Sources/Controllers/lessonController.php
@@ -24,20 +24,42 @@
                 echo json_encode($data);
             }else{
                 if(!$this->m->_path[3]){    //создаем новую сессию
-                    
+                    $row->user_id = $this->m->_user->id;
+                    $row->lesson_id = $this->m->_path[2];
+                    $row->uuid = md5(time());
+                    $row->date = date("Y-m-d H:i:s");
+                    $row->status = 1;
+                    if($this->m->_db->insertObject('sessions',$row)){
+                        
+                        redirect('/lesson/show/'.$this->m->_path[2].'/'.$row->uuid);
+                    }
                 }else{  //проверяем или она дествует
-                    $this->m->_db->setQuery();
+                    $this->m->_db->setQuery(
+                                "SELECT COUNT(*) as cnt "
+                                . " FROM `sessions` WHERE `sessions`.`lesson_id` = ".(int)$this->m->_path[2]
+                                . " AND `sessions`.`user_id` = ".$this->m->_user->id
+                                . " AND `sessions`.`uuid` = '".$this->m->_path[3]."'"
+                                . " LIMIT 1"
+                            );
+                    $session = $this->m->_db->loadResult();
+                    
+                    if($session){
+                        //получаем елементы для уровня
+                        $this->m->_db->setQuery(
+                                    "SELECT `questions`.* "
+                                    . " FROM `questions` "
+                                    . " WHERE `questions`.`lesson_id` = 1"
+                                    . " AND `questions`.`status` = 1"
+
+                                );
+                        $this->m->data = $this->m->_db->loadObjectList();                                        
+                        
+                    }else{
+                        //redirect('/level/');
+                    }
                 }
                 
-                //получаем елементы для уровня
-                $this->m->_db->setQuery(
-                            "SELECT `questions`.* "
-                            . " FROM `questions` "
-                            . " WHERE `questions`.`lesson_id` = 1"
-                            . " AND `questions`.`status` = 1"
-
-                        );
-                $this->m->data = $this->m->_db->loadObjectList();                
+                
             }
         }
     }
---
+++ language.sql
@@ -0,0 +1,257 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.0.2
+-- http://www.phpmyadmin.net
+--
+-- Хост: localhost
+-- Время создания: Мар 07 2017 г., 23:18
+-- Версия сервера: 5.6.26-log
+-- Версия PHP: 5.6.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `language`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `lessons`
+--
+
+CREATE TABLE `lessons` (
+  `id` int(11) NOT NULL,
+  `level` int(11) NOT NULL DEFAULT '0',
+  `type` tinyint(2) NOT NULL DEFAULT '0',
+  `order` tinyint(3) NOT NULL DEFAULT '0',
+  `status` int(11) NOT NULL,
+  `date` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `lessons`
+--
+
+INSERT INTO `lessons` (`id`, `level`, `type`, `order`, `status`, `date`) VALUES
+(1, 1, 1, 1, 1, '0000-00-00 00:00:00'),
+(2, 1, 1, 2, 1, '0000-00-00 00:00:00'),
+(3, 1, 1, 3, 1, '0000-00-00 00:00:00');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `levels`
+--
+
+CREATE TABLE `levels` (
+  `id` int(11) NOT NULL,
+  `order` tinyint(5) NOT NULL DEFAULT '0',
+  `status` tinyint(2) NOT NULL DEFAULT '1',
+  `date` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `levels`
+--
+
+INSERT INTO `levels` (`id`, `order`, `status`, `date`) VALUES
+(1, 1, 1, '0000-00-00 00:00:00');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `questions`
+--
+
+CREATE TABLE `questions` (
+  `id` int(11) NOT NULL,
+  `lesson_id` int(11) NOT NULL,
+  `element` text NOT NULL,
+  `type` tinyint(4) NOT NULL DEFAULT '1',
+  `use` tinyint(2) NOT NULL DEFAULT '1',
+  `status` tinyint(2) DEFAULT '1',
+  `date` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `questions`
+--
+
+INSERT INTO `questions` (`id`, `lesson_id`, `element`, `type`, `use`, `status`, `date`) VALUES
+(1, 1, 'G', 1, 1, 1, '0000-00-00 00:00:00'),
+(2, 1, 'L', 1, 0, 1, '0000-00-00 00:00:00'),
+(3, 1, 'F', 1, 1, 1, '0000-00-00 00:00:00'),
+(4, 1, 'P', 1, 0, 1, '0000-00-00 00:00:00'),
+(5, 1, 'I', 1, 1, 1, '0000-00-00 00:00:00'),
+(6, 1, 'A', 1, 1, 1, '0000-00-00 00:00:00');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `sessions`
+--
+
+CREATE TABLE `sessions` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL DEFAULT '0',
+  `lesson_id` int(11) NOT NULL DEFAULT '0',
+  `uuid` varchar(255) NOT NULL DEFAULT '',
+  `date` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `status` tinyint(2) DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `sessions`
+--
+
+INSERT INTO `sessions` (`id`, `user_id`, `lesson_id`, `uuid`, `date`, `status`) VALUES
+(1, 1, 1, '0098f1d6b31c4cf17eaaf351c8467574', '2017-03-07 21:16:34', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `firstname` varchar(250) NOT NULL DEFAULT '',
+  `lastname` varchar(250) NOT NULL DEFAULT '',
+  `fathersname` varchar(255) NOT NULL DEFAULT '',
+  `password` varchar(250) NOT NULL DEFAULT '',
+  `country` int(11) DEFAULT NULL,
+  `email` varchar(250) NOT NULL DEFAULT '',
+  `birthday` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `status` tinyint(4) NOT NULL DEFAULT '1',
+  `bad_auth` tinyint(2) NOT NULL DEFAULT '0',
+  `bad_auth_time` int(11) DEFAULT NULL,
+  `bad_withdraw_answer` tinyint(1) NOT NULL DEFAULT '0',
+  `last_login` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `last_modified` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `last_ip` varchar(250) DEFAULT NULL,
+  `date` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `users`
+--
+
+INSERT INTO `users` (`id`, `firstname`, `lastname`, `fathersname`, `password`, `country`, `email`, `birthday`, `status`, `bad_auth`, `bad_auth_time`, `bad_withdraw_answer`, `last_login`, `last_modified`, `last_ip`, `date`) VALUES
+(1, 'Slawik', 'Sivinyuk', '', 'dd8275800e16216360194ae9ab4a0eb2:1MHyW0kH7zbhfiqM', NULL, '279229931@qip.ru', '0000-00-00 00:00:00', 1, 0, NULL, 0, '2017-03-07 21:15:43', '0000-00-00 00:00:00', '127.0.0.1', '2017-03-05 20:34:10');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `x_session`
+--
+
+CREATE TABLE `x_session` (
+  `id` int(11) NOT NULL,
+  `session_id` varchar(45) DEFAULT NULL,
+  `time` int(11) DEFAULT NULL,
+  `userid` int(11) NOT NULL DEFAULT '0',
+  `device_id` varchar(255) NOT NULL DEFAULT '',
+  `username` varchar(255) NOT NULL DEFAULT '',
+  `guest` tinyint(4) NOT NULL DEFAULT '1',
+  `guest_key` varchar(50) NOT NULL DEFAULT '',
+  `usertype` varchar(50) NOT NULL DEFAULT '',
+  `status` int(2) DEFAULT '1',
+  `remember` tinyint(4) NOT NULL DEFAULT '0',
+  `gid` int(10) NOT NULL DEFAULT '1',
+  `ip` varchar(20) DEFAULT NULL,
+  `user_agent` varchar(255) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `x_session`
+--
+
+INSERT INTO `x_session` (`id`, `session_id`, `time`, `userid`, `device_id`, `username`, `guest`, `guest_key`, `usertype`, `status`, `remember`, `gid`, `ip`, `user_agent`) VALUES
+(15, '5922f6f87932736aa75d6c06258371ce', 1488921415, 1, '', '279229931@qip.ru', 0, '', 'user', 1, 0, 0, '127.0.0.1', 'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:51.0) Gecko/20100101 Firefox/51.0');
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `lessons`
+--
+ALTER TABLE `lessons`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `levels`
+--
+ALTER TABLE `levels`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `questions`
+--
+ALTER TABLE `questions`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `sessions`
+--
+ALTER TABLE `sessions`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `email` (`email`);
+
+--
+-- Индексы таблицы `x_session`
+--
+ALTER TABLE `x_session`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `session` (`session_id`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `lessons`
+--
+ALTER TABLE `lessons`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT для таблицы `levels`
+--
+ALTER TABLE `levels`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT для таблицы `questions`
+--
+ALTER TABLE `questions`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+--
+-- AUTO_INCREMENT для таблицы `sessions`
+--
+ALTER TABLE `sessions`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT для таблицы `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT для таблицы `x_session`
+--
+ALTER TABLE `x_session`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
